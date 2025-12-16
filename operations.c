1ac910ca0f9b6d03a50848041cab5b3c24289c56
Sasha22D <sasha.daniel2006@gmail.com>
2025-12-16 15:05:29 +0100

    Creation fichier operations.c

---
+++ operations.c
@@ -0,0 +1,27 @@
+#include "push_swap.h"
+
+void	swap_first_elements(t_stack_node **stack)
+{
+	t_stack_node	*head;
+	long	cpy_value;
+
+	if (*stack && (*stack)->next)
+	{
+		head = *stack;
+		cpy_value = head->value;
+		head->value = head->next->value;
+		head->next->value = cpy_value;
+	}
+
+}
+
+void	ss(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	swap_first_elements(stack_a);
+	swap_first_elements(stack_b);
+}
+
+void	push_first_element(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	
+}

248a5ea00cd665c5c549abf54906f939f5298629
Sasha22D <sasha.daniel2006@gmail.com>
2025-12-17 13:56:30 +0100

    premiere version de reverse rotate commands a l'air de fonctionner

---
+++ reverse_rotate_commands.c
@@ -0,0 +1,29 @@
+#include "push_swap.h"
+
+void	reverse_rotate_stack(t_stack_node **stack)
+{
+	t_stack_node	*head;
+	t_stack_node	*tail;
+	t_stack_node	*second;
+	t_stack_node	*temp;
+
+	if (!(*stack)->next)
+		return ;
+	head = *stack;
+	tail = *stack;
+	second = head->next;
+	temp = second;
+	while (tail->next)
+		tail = tail->next;
+	second->prev = NULL;
+	head->prev = tail;
+	head->next = NULL;
+	head->index = tail->index + 1;
+	tail->next = head;
+	while (temp != NULL)
+	{
+		temp->index -= 1;
+		temp = temp->next;
+	}
+	*stack = second;
+}

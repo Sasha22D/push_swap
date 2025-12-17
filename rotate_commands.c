869b3fea3d96bb7f814cb72426f244e986301e4a
Sasha22D <sasha.daniel2006@gmail.com>
2025-12-17 12:42:29 +0100

    Premiere version de rotate_commands: SEGFAULT

--- rotate_commands.c
+++ rotate_commands.c
@@ -1,2 +1,22 @@
 #include "push_swap.h"
 
+void	rotate_stack(t_stack_node **stack)
+{
+	t_stack_node	*head;
+	t_stack_node	*tail;
+
+	head = *stack;
+	tail = *stack;
+	while (tail != NULL)
+		tail = tail->next;
+	tail->prev->next = NULL;
+	tail->prev = NULL;
+	tail->next = head;
+	head->prev = tail;
+	while (head != NULL)
+	{
+		head->index += 1;
+		head = head->next;
+	}
+	*stack = tail;
+}

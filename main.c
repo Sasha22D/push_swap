a92f11ffbc9f7cbffc7f23f0df3582e13f3e66db
Sasha22D <sasha.daniel2006@gmail.com>
2025-12-18 13:43:38 +0100

    mise a jour du main et du header pour le check stack/tiny sort

--- main.c
+++ main.c
@@ -3,15 +3,30 @@
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
-	t_stack_node	*b;
+	// t_stack_node	*b;
 
 	a = NULL;
-	b = NULL;
+	// b = NULL;
 	if (argc == 1 || (argc == 2 && argv[1][0] == 0))
 		return (1);
 	if (argc == 2)
+	{
 		argv = ft_split(argv[1]);
-	ft_init_stack(&a, argv);
+		ft_init_stack(&a, argv);
+	}
+	else
+		ft_init_stack(&a, argv + 1);
+	if (!is_stack_sorted(&a))
+	{
+		if (get_stack_len(&a) == 2)
+		{
+			swap_first_elements(&a);
+		}
+		else if (get_stack_len(&a) == 3)
+		{
+			tiny_sort(&a);
+		}
+	}
 	// while (a != NULL)
 	// {
 	// 	printf("Value: %ld, index: %d\n", a->value, a->index);
--- push_swap.h
+++ push_swap.h
@@ -21,8 +21,19 @@ char	**ft_split(char *str);
 void	ft_init_stack(t_stack_node **stack, char **tab);
 void	ft_lst_add_front(t_stack_node **stack, t_stack_node *new);
 void	ft_lstdelone(t_stack_node *node);
+// SWAP FIRST ELEMENTS COMMANDS
 void	swap_first_elements(t_stack_node **stack);
 void	ss(t_stack_node **stack_a, t_stack_node **stack_b);
+// PUSH FIRST ELEMENT COMMAND
 void	push_first_element(t_stack_node **stack_1, t_stack_node **stack_2);
+// ROTATE COMMANDS
+void	rotate_stack(t_stack_node **stack);
+// REVERSE ROTATE COMMANDS
+void	reverse_rotate_stack(t_stack_node **stack);
+// CHECK STACK AND TINY SORT
+int	is_stack_sorted(t_stack_node **stack);
+int	get_stack_len(t_stack_node **stack);
+t_stack_node	*find_biggest(t_stack_node **stack);
+void	tiny_sort(t_stack_node **stack);
 
 #endif

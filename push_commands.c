5528d28c6b0535bcffe70b5981aefbf3e632ef0c
Sasha Daniel <sadaniel@f0r1s4.clusters.42paris.fr>
2025-12-31 15:56:34 +0100

    Normed push_commands

--- push_commands.c
+++ push_commands.c
@@ -17,32 +17,23 @@ void	push_first_element(t_stack_node **stack_1, t_stack_node **stack_2)
 	t_stack_node	*head;
 
 	temp = *stack_1;
-	if ((*stack_1)->next)
-	{
-		*stack_1 = (*stack_1)->next;
-		head = *stack_1;
-	}
-	else
-		head = NULL;
+	*stack_1 = temp->next;
 	temp->prev = NULL;
 	temp->next = *stack_2;
 	*stack_2 = temp;
-	*stack_1 = head;
+	if ((*stack_2)->next)
+		(*stack_2)->next->prev = *stack_2;
+	head = *stack_1;
 	while (head != NULL)
 	{
 		head->index -= 1;
 		head = head->next;
 	}
-	head = *stack_2;
-	if (head->next)
+	head = (*stack_2)->next;
+	while (head != NULL)
 	{
+		head->index += 1;
 		head = head->next;
-		head->prev = *stack_2;
-		while (head != NULL)
-		{
-			head->index += 1;
-			head = head->next;
-		}
 	}
 }
 
